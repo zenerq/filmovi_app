0f12196b00c70d7990f3748d141cabcf1c3ae538
zener <rokosamje@gmail.com>
2022-07-15 22:51:47 +0200

    temelj

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { CssBaseline } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+
+import useStyles from "./styles";
+
+import { Actors, MovieInformation, Movies, NavBar, Profile } from "./";
 
 const App = () => {
-  <div>App</div>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route exact path="/movie/:id" element={<MovieInformation />}></Route>
+          <Route exact path="/actors/:id" element={<Actors />}></Route>
+          <Route exact path="/" element={<Movies />}></Route>
+          <Route exact path="/profile/:id" element={<Profile />}></Route>
+        </Routes>
+      </main>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const MovieInformation = () => {
+  console.log("Movie information");
+  return <div>Movie Information</div>;
+};
+
+export default MovieInformation;
